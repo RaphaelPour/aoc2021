65f2d2359363c15c6258075f4c40b203538ebc6a
Raphael Pour <info@raphaelpour.de>
2021-12-14 17:51:45 +0100

    days 14: trysolve part 2

--- days/14/part.go
+++ days/14/part.go
@@ -12,12 +12,14 @@ type RuleSet struct {
 	start         string
 	rules         map[string]string
 	realCacheHits int
+	pairMap       map[string]int
 }
 
 func NewRuleSet(input []string) *RuleSet {
 	r := new(RuleSet)
 	r.start = input[0]
 	r.rules = make(map[string]string)
+	r.pairMap = make(map[string]int)
 	for _, rawRule := range input[2:] {
 		parts := strings.Split(rawRule, " -> ")
 		if len(parts) != 2 {
@@ -28,6 +30,12 @@ func NewRuleSet(input []string) *RuleSet {
 		}
 		r.rules[parts[0]] = parts[1]
 	}
+
+	for i := 0; i < len(r.start)-1; i++ {
+		pair := r.start[i : i+2]
+		r.pairMap[pair] = 1
+	}
+
 	return r
 }
 
@@ -41,11 +49,31 @@ func (rs *RuleSet) SubstituteOnce() {
 		}
 		newStart += conclusion + string(rs.start[i+1])
 	}
-	fmt.Printf("%s -> %s\n", rs.start, newStart)
+	// fmt.Printf("%s -> %s\n", rs.start, newStart)
 	rs.start = newStart
 }
 
-func (rs *RuleSet) SubstituteFast() {
+func (rs *RuleSet) SubstituteFast2() {
+	newPairs := make(map[string]int)
+	for premise, conclusion := range rs.rules {
+		if _, ok := rs.pairMap[premise]; !ok {
+			// rule doesn't match
+			continue
+		}
+
+		pair1 := string(premise[0]) + conclusion
+		pair2 := conclusion + string(premise[1])
+
+		newPairs[pair1] = newPairs[pair1] + rs.pairMap[premise]
+		if pair1 != pair2 {
+			newPairs[pair2] = newPairs[pair2] + rs.pairMap[premise]
+		}
+	}
+
+	rs.pairMap = newPairs
+}
+
+func (rs *RuleSet) SubstituteFast(expandCache bool) {
 	newStart := string(rs.start[0])
 	oldStart := rs.start
 	for len(oldStart) >= 2 {
@@ -63,20 +91,8 @@ func (rs *RuleSet) SubstituteFast() {
 			// CACHE HIT!
 			if len(string(oldStart[:i])) > 2 {
 				rs.realCacheHits++
-
-				fmt.Printf("cache hit: %s -> %s\n", string(oldStart[:i]), conclusion)
-				/*
-					fmt.Printf(
-						"           expand output: %s -> %s\n",
-						newStart, newStart+conclusion+string(oldStart[1]),
-					)
-					fmt.Printf(
-						"            reduce input: %s -> %s\n",
-						oldStart,
-						string(oldStart[i-1:]),
-					)*/
 			}
-			newStart += conclusion + string(oldStart[1])
+			newStart += conclusion + string(oldStart[len(conclusion)/2+1])
 			oldStart = string(oldStart[i-1:])
 			break
 		}
@@ -85,23 +101,28 @@ func (rs *RuleSet) SubstituteFast() {
 	// fmt.Printf("%s -> %s\n", rs.start, newStart)
 
 	// cache result for later
-
-	/*
-		for window := 4; window < len(rs.start); window += 2 {
-			for i := 0; i < len(rs.start); i += window {
-				newPremise := rs.start[i : i+window]
-				if _, ok := rs.rules[newPremise]; ok {
-					// fmt.Printf("rule for %s already existing\n", rs.start[:i])
-					continue
-				}
-				newConclusion := newStart[window/2 : (i+window)-window/2]
-				rs.rules[newPremise] = newConclusion
-				fmt.Printf("New rule %s -> %s\n", newPremise, newConclusion)
-			}
-		}*/
-	// fmt.Printf("|rules| = %d\n", len(rs.rules))
-
+	if expandCache {
+		rs.EnrichCache(rs.start, newStart)
+	}
 	rs.start = newStart
+}
+
+func (rs *RuleSet) EnrichCache(original, substitute string) {
+	// for each window size
+	for windowSize := 4; windowSize <= len(original)/2; windowSize += 2 {
+		// for each window in input
+		for windowOffset := 0; windowOffset < len(original)-windowSize+1; windowOffset++ {
+			premise := original[windowOffset : windowOffset+windowSize]
+			if _, ok := rs.rules[premise]; ok {
+				continue
+			}
+
+			conclusionIndex := windowOffset * 2
+			conclusionSize := (2*windowSize - 1)
+			conclusion := substitute[conclusionIndex+1 : conclusionIndex+conclusionSize-1]
+			rs.rules[premise] = conclusion
+		}
+	}
 }
 
 func (rs RuleSet) ElementStats() (int, int) {
@@ -131,13 +152,13 @@ func (rs RuleSet) Result() int {
 func part1(input []string) int {
 	ruleSet := NewRuleSet(input)
 
-	for round := 0; round < 7; round++ {
+	for round := 0; round < 10; round++ {
 		start := time.Now()
 		ruleSet.SubstituteOnce()
 
 		fmt.Printf(
 			"round %d in %s with result %d\n",
-			round,
+			round+1,
 			time.Since(start),
 			ruleSet.Result(),
 		)
@@ -150,37 +171,47 @@ func part1(input []string) int {
 
 func part2(input []string) int {
 	ruleSet := NewRuleSet(input)
-	ruleSetTest := NewRuleSet(input)
 
-	for round := 0; round < 7; round++ {
+	for round := 0; round < 40; round++ {
 		start := time.Now()
-		ruleSet.SubstituteFast()
-		ruleSetTest.SubstituteOnce()
+		ruleSet.SubstituteFast2()
 
 		// fmt.Printf("round %d in %s with output %s\n", round, time.Since(start), ruleSet.start)
 		fmt.Printf(
-			"round %d in %s with result %d and %d real cache hits\n",
-			round,
+			"round %d in %s\n",
+			round+1,
 			time.Since(start),
-			ruleSet.Result(),
-			ruleSet.realCacheHits,
 		)
-		if ruleSet.start != ruleSetTest.start {
-			fmt.Printf("start differs:\n  valid: %s\ninvalid: %s\n", ruleSetTest.start, ruleSet.start)
-			break
+	}
+
+	hist := make(map[string]int)
+	hist[string(ruleSet.start[0])] = hist[string(ruleSet.start[0])] + 1
+	for pair, sum := range ruleSet.pairMap {
+		// hist[string(pair[0])] = hist[string(pair[0])] + sum
+		hist[string(pair[1])] = hist[string(pair[1])] + sum
+	}
+
+	min, max := hist[string(ruleSet.start[0])], hist[string(ruleSet.start[0])]
+	for _, sum := range hist {
+		if sum > max {
+			max = sum
+		}
+		if sum < min {
+			min = sum
 		}
 	}
 
-	min, max := ruleSet.ElementStats()
-	fmt.Printf("min= %d, max=%d\n", min, max)
+	fmt.Printf("min=%d, max=%d\n", min, max)
 	return max - min
 }
 
 func main() {
-	input := "input_example"
+	input := "input"
 	fmt.Println("== [ PART 1 ] ==")
 	fmt.Println(part1(util.LoadString(input)))
 
 	fmt.Println("== [ PART 2 ] ==")
 	fmt.Println(part2(util.LoadString(input)))
+	fmt.Println("too high: 4288726369828")
+	fmt.Println("bad: 3277772741531")
 }
--- days/14/part_test.go
+++ days/14/part_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/RaphaelPour/aoc2021/util"
@@ -38,31 +39,70 @@ func TestSubstituteOnce(t *testing.T) {
 }
 
 func TestSubstituteFast(t *testing.T) {
+	t.Skip()
 	rs := NewRuleSet(util.LoadString("input_example"))
 	require.Equal(t, "NNCB", rs.start)
-	rs.SubstituteFast()
+	rs.SubstituteFast(true)
 	require.Equal(t, "NCNBCHB", rs.start)
 	require.Equal(t, 1, rs.Result())
-	rs.SubstituteFast()
+	rs.SubstituteFast(true)
 	require.Equal(t, "NBCCNBBBCBHCB", rs.start)
 	require.Equal(t, 5, rs.Result())
-	rs.SubstituteFast()
+	rs.SubstituteFast(true)
 	require.Equal(t, "NBBBCNCCNBBNBNBBCHBHHBCHB", rs.start)
 	require.Equal(t, 7, rs.Result())
-	rs.SubstituteFast()
+	rs.SubstituteFast(true)
 	require.Equal(t, "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB", rs.start)
 	require.Equal(t, 18, rs.Result())
-	rs.SubstituteFast()
+	rs.SubstituteFast(true)
 	require.Equal(t, 33, rs.Result())
-	rs.SubstituteFast()
+	rs.SubstituteFast(true)
 	require.Equal(t, 82, rs.Result())
-	rs.SubstituteFast()
+	rs.SubstituteFast(true)
 	require.Equal(t, 160, rs.Result())
-	rs.SubstituteFast()
+	rs.SubstituteFast(true)
 	require.Equal(t, 366, rs.Result())
-	rs.SubstituteFast()
+	rs.SubstituteFast(true)
 	require.Equal(t, 727, rs.Result())
-	rs.SubstituteFast()
+	rs.SubstituteFast(true)
 	require.Equal(t, 1588, rs.Result())
-	rs.SubstituteFast()
+	rs.SubstituteFast(true)
+}
+
+func TestCache1(t *testing.T) {
+	r := new(RuleSet)
+	r.rules = make(map[string]string)
+
+	r.EnrichCache("ABCD", "AaBbCcD")
+	fmt.Println(r.rules)
+	require.Equal(t, 1, len(r.rules))
+	require.Equal(t, "aBbCc", r.rules["ABCD"])
+}
+
+func TestCache2(t *testing.T) {
+	r := new(RuleSet)
+	r.rules = make(map[string]string)
+
+	r.EnrichCache("ABCDE", "AaBbCcDdE")
+	fmt.Println(r.rules)
+	require.Equal(t, 2, len(r.rules))
+	require.Equal(t, "aBbCc", r.rules["ABCD"])
+	require.Equal(t, "bCcDd", r.rules["BCDE"])
+}
+
+func TestCache3(t *testing.T) {
+	r := new(RuleSet)
+	r.rules = make(map[string]string)
+
+	r.EnrichCache("ABCDEF", "AaBbCcDdEeF")
+	fmt.Println(r.rules)
+	require.Equal(t, 4, len(r.rules))
+	require.Equal(t, "aBbCc", r.rules["ABCD"])
+	require.Equal(t, "bCcDd", r.rules["BCDE"])
+	require.Equal(t, "cDdEe", r.rules["CDEF"])
+	require.Equal(t, "aBbCcDdEe", r.rules["ABCDEF"])
+
+	r.start = "ABCDEF"
+	r.SubstituteFast(false)
+	require.Equal(t, "AaBbCcDdEeF", r.start)
 }
