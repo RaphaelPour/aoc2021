bf602ac648f5091b7abfc0708931731527a5310b
Raphael Pour <info@raphaelpour.de>
2021-12-19 14:05:53 +0100

    days 19: add

---
+++ days/19/part.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/RaphaelPour/aoc2021/util"
+)
+
+func part1() {
+	// input := util.LoadString("input")
+	// input := util.LoadDefaultInt()
+	// input := util.LoadInt("input")
+	input := util.LoadDefaultString()
+}
+
+func part2() {
+
+}
+
+func main() {
+    fmt.Println("== [ PART 1 ] ==")
+    part1()
+
+    fmt.Println("== [ PART 2 ] ==")
+    part2()
+}
