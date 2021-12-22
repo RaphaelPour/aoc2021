261e754ebaf25bf62098774b3b244f4357741a0d
Raphael Pour <info@raphaelpour.de>
2021-12-22 06:43:53 +0100

    Util: add toInt fast conversion

--- util/string.go
+++ util/string.go
@@ -1,9 +1,22 @@
 package util
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func Reverse(in string) string {
 	result := ""
 	for _, char := range in {
 		result = string(char) + result
 	}
 	return result
+}
+
+func ToInt(in string) int {
+	out, err := strconv.Atoi(in)
+	if err != nil {
+		panic(fmt.Sprintf("error converting %s to int: %s", in, err))
+	}
+	return out
 }
