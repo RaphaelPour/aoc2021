a4131aa1935db9430526f6d53b128ad8fb8f1f04
Raphael Pour <info@raphaelpour.de>
2021-12-25 07:18:37 +0100

    days 25: remove obsolete codedays 25: remove obsolete code

--- days/25/part.go
+++ days/25/part.go
@@ -42,36 +42,12 @@ func NewSeaFloor(input []string) *SeaFloor {
 	return s
 }
 
-func (s SeaFloor) HasEastNeighbour(x, y int) bool {
-	neighborX := x + 1
-	// wrap x
-	if neighborX >= len(s.fields[0]) {
-		neighborX = 0
-	}
-
-	return s.fields[y][neighborX] != EMPTY
-}
-
-func (s SeaFloor) HasSoutheighbour(x, y int) bool {
-	neighborY := y + 1
-	// wrap y
-	if neighborY >= len(s.fields) {
-		neighborY = 0
-	}
-
-	return s.fields[neighborY][x] != EMPTY
-}
-
 func (s *SeaFloor) MoveLoop() int {
-
 	var round int
 	anyMove := true
 	for anyMove {
 		newFields := make([][]int, len(s.fields))
 		anyMove = false
-
-		//fmt.Printf(" --- %d ---\n", round)
-		// s.Dump()
 
 		// process east
 		for y := range s.fields {
@@ -89,9 +65,7 @@ func (s *SeaFloor) MoveLoop() int {
 				if s.fields[y][nextX] == EMPTY {
 					newFields[y][nextX] = EAST
 					anyMove = true
-					// fmt.Printf("move east %d,%d -> %d,%d\n", x, y, nextX, y)
 				} else {
-					// fmt.Printf("leave east %d,%d\n", x, y)
 					newFields[y][x] = EAST
 				}
 			}
@@ -112,18 +86,13 @@ func (s *SeaFloor) MoveLoop() int {
 				if s.fields[nextY][x] != SOUTH && newFields[nextY][x] != EAST {
 					newFields[nextY][x] = SOUTH
 					anyMove = true
-					//fmt.Printf("move south %d,%d -> %d,%d\n", x, y, x, nextY)
 				} else {
 					newFields[y][x] = SOUTH
-					//fmt.Printf("leave south %d,%d\n", x, y)
 				}
 			}
 		}
 		s.fields = newFields
 		round++
-		if round%100 == 0 {
-			fmt.Print(".")
-		}
 	}
 	return round
 }
@@ -146,15 +115,8 @@ func part1(input []string) int {
 	return s.MoveLoop()
 }
 
-func part2() {
-
-}
-
 func main() {
 	input := "input"
 	fmt.Println("== [ PART 1 ] ==")
 	fmt.Println(part1(util.LoadString(input)))
-
-	fmt.Println("== [ PART 2 ] ==")
-	part2()
 }
