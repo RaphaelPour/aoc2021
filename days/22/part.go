04f1818f914133628e0f2feac71371af5a082892
Raphael Pour <info@raphaelpour.de>
2021-12-29 07:54:07 +0100

    days 22: solve part 2 :tada:

--- days/22/part.go
+++ days/22/part.go
@@ -7,139 +7,170 @@ import (
 	"github.com/RaphaelPour/aoc2021/util"
 )
 
-type Point struct {
-	x, y, z int
-}
-
-func (p Point) String() string {
-	return fmt.Sprintf("%d|%d|%d", p.x, p.y, p.z)
-}
+/*
+ * idea from https://github.com/goggle/AdventOfCode2021.jl/blob/master/src/day22.jl
+ *
+ * 1) parse each line as cubeoid having from,to vector3's and a on/off state
+ * 2) intersect each parsed cuboid with already parsed ones:
+ * 2.1) if intersect: add intersection cuboid with off-state to the cuboid list
+ * 3) if cuboid has on-state: add it to cuboid list
+ */
 
 type Range struct {
 	from, to int
 }
 
-func (r Range) Intersection(other Range) (*Range, bool) {
-	if other.from > r.to {
+func (r Range) StartBehind(other Range) bool {
+	return other.from > r.to
+}
+
+/* intersection of two sets https://scicomp.stackexchange.com/a/26260 */
+func (r Range) Intersect(other Range) (*Range, bool) {
+	if r.StartBehind(other) || other.StartBehind(r) {
 		return nil, false
 	}
 
 	return &Range{
-		from: util.Max(other.from, r.from),
-		to:   util.Min(other.to, r.to),
+		from: util.Max(r.from, other.from),
+		to:   util.Min(r.to, other.to),
 	}, true
+}
+
+func (r Range) Length() int {
+	/* add one since the end of the interval is also contained in the
+	 * count of cuboids
+	 */
+	return util.Abs(r.to-r.from) + 1
 }
 
 func (r Range) String() string {
 	return fmt.Sprintf("%d..%d", r.from, r.to)
 }
 
-type RebootStep struct {
+type Cube struct {
 	x, y, z Range
 	on      bool
 }
 
-func (r RebootStep) String() string {
-	status := "off"
-	if r.on {
-		status = "on"
+func (c Cube) Intersection(other Cube) (*Cube, bool) {
+	newX, ok := c.x.Intersect(other.x)
+	if !ok {
+		return nil, false
 	}
-	return fmt.Sprintf("%s x=%s,y=%s,z=%s", status, r.x, r.y, r.z)
+
+	newY, ok := c.y.Intersect(other.y)
+	if !ok {
+		return nil, false
+	}
+
+	newZ, ok := c.z.Intersect(other.z)
+	if !ok {
+		return nil, false
+	}
+
+	return &Cube{
+		x: *newX,
+		y: *newY,
+		z: *newZ,
+	}, true
 }
 
-type Cubes struct {
-	cubeMap     map[Point]bool
-	rebootSteps []RebootStep
+func (c Cube) String() string {
+	return fmt.Sprintf("%s|%s|%s", c.x, c.y, c.z)
 }
 
-func NewCubes(input []string) *Cubes {
-	c := new(Cubes)
-	c.cubeMap = make(map[Point]bool)
-	c.rebootSteps = make([]RebootStep, len(input))
+func (c Cube) Volume() int {
+	coeff := 1
+	if !c.on {
+		coeff = -1
+	}
+	return coeff * c.x.Length() * c.y.Length() * c.z.Length()
+}
+
+type Cubes []Cube
+
+func (c Cubes) Volume() int {
+	sum := 0
+	for _, cube := range c {
+		sum += cube.Volume()
+	}
+	return sum
+}
+
+func NewCubes(input []string, crop bool) Cubes {
 	pattern := regexp.MustCompile(`(on|off) x=(-?\d+)..(-?\d+),y=(-?\d+)..(-?\d+),z=(-?\d+)..(-?\d+)`)
-	for i, line := range input {
+	cubes := make(Cubes, 0)
+	for _, line := range input {
 		match := pattern.FindStringSubmatch(line)
 		if len(match) != 8 {
 			panic(fmt.Sprintf("error parsing %s", line))
 		}
 
-		var on bool
-		if match[1] == "on" {
-			on = true
+		cube := Cube{
+			on: match[1] == "on",
+			x: Range{
+				util.ToInt(match[2]),
+				util.ToInt(match[3]),
+			},
+			y: Range{
+				util.ToInt(match[4]),
+				util.ToInt(match[5]),
+			},
+			z: Range{
+				util.ToInt(match[6]),
+				util.ToInt(match[7]),
+			},
 		}
 
-		xFrom := util.ToInt(match[2])
-		xTo := util.ToInt(match[3])
-		yFrom := util.ToInt(match[4])
-		yTo := util.ToInt(match[5])
-		zFrom := util.ToInt(match[6])
-		zTo := util.ToInt(match[7])
-
-		c.rebootSteps[i] = RebootStep{
-			Range{xFrom, xTo},
-			Range{yFrom, yTo},
-			Range{zFrom, zTo},
-			on,
-		}
-
-		if util.Abs(xFrom) >= 50 || util.Abs(xTo) > 50 {
-			continue
-		}
-
-		if util.Abs(yFrom) >= 50 || util.Abs(yTo) > 50 {
-			continue
-		}
-
-		if util.Abs(zFrom) >= 50 || util.Abs(zTo) > 50 {
-			continue
-		}
-
-		for z := zFrom; z <= zTo; z++ {
-			for y := yFrom; y <= yTo; y++ {
-				for x := xFrom; x <= xTo; x++ {
-					if on {
-						c.cubeMap[Point{x, y, z}] = false
-					} else {
-						delete(c.cubeMap, Point{x, y, z})
-					}
-				}
+		if crop {
+			if util.Abs(cube.x.from) >= 50 || util.Abs(cube.x.to) > 50 {
+				continue
 			}
-		}
-	}
-
-	return c
-}
-
-func (c *Cubes) Reboot() {
-	for _, step := range c.rebootSteps {
-		fmt.Printf("Processing step %s\n", step)
-		for z := step.z.from; z <= step.z.to; z++ {
-			for y := step.y.from; y <= step.y.to; y++ {
-				for x := step.x.from; x <= step.x.to; x++ {
-					c.cubeMap[Point{x, y, z}] = step.on
-				}
+			if util.Abs(cube.y.from) >= 50 || util.Abs(cube.y.to) > 50 {
+				continue
+			}
+			if util.Abs(cube.z.from) >= 50 || util.Abs(cube.z.to) > 50 {
+				continue
 			}
 		}
 
-	}
-}
+		oldLength := len(cubes)
+		for i := 0; i < oldLength; i++ {
+			intersection, ok := cube.Intersection(cubes[i])
+			if ok {
+				/* use inverted state from cube from the list:
+				 * cube: we want to subtract the intersection since two cubes
+				 *       would count the intersection area twice
+				 * hole: add intersection again
+				 */
+				intersection.on = !cubes[i].on
+				cubes = append(cubes, *intersection)
+			}
+		}
 
-func (c Cubes) CubesOnCount() int {
-	count := 0
-	for _, ok := range c.cubeMap {
-		if ok {
-			count++
+		if cube.on {
+			cubes = append(cubes, cube)
 		}
 	}
-	return count
+
+	return cubes
 }
 
 func part1(input []string) int {
-	return len(NewCubes(input).cubeMap)
+	c := NewCubes(input, true)
+	return c.Volume()
+}
+
+func part2(input []string) int {
+	c := NewCubes(input, false)
+	return c.Volume()
 }
 
 func main() {
 	input := "input"
 	fmt.Println("== [ PART 1 ] ==")
 	fmt.Println(part1(util.LoadString(input)))
+
+	fmt.Println("== [ PART 2 ] ==")
+	fmt.Println(part2(util.LoadString(input)))
 }
--- days/22/part_test.go
+++ days/22/part_test.go
@@ -3,19 +3,151 @@ package main
 import (
 	"testing"
 
+	"github.com/RaphaelPour/aoc2021/util"
 	"github.com/stretchr/testify/require"
 )
 
+func TestExample1Part1(t *testing.T) {
+	require.Equal(t, 39, part1(util.LoadString("input_example1")))
+}
+
+func TestExample3Part1(t *testing.T) {
+	require.Equal(t, 27, part1(util.LoadString("input_example3")))
+}
+
+func TestPart1(t *testing.T) {
+	require.Equal(t, 601104, part1(util.LoadString("input")))
+}
+
+func TestPart2(t *testing.T) {
+	require.Equal(t, 1262883317822267, part2(util.LoadString("input")))
+}
+
 func TestVolume(t *testing.T) {
 	c := Cube{
-		from: Vector3{0, 0, 0},
-		to:   Vector3{2, 2, 2},
+		on: true,
+		x:  Range{0, 2},
+		y:  Range{0, 2},
+		z:  Range{0, 2},
 	}
-	require.Equal(t, 8, c.Volume())
+	require.Equal(t, 27, c.Volume())
 
 	c = Cube{
-		from: Vector3{-10, -1, 0},
-		to:   Vector3{-8, 1, 2},
+		on: false,
+		x:  Range{-10, -8},
+		y:  Range{-1, 1},
+		z:  Range{0, 2},
 	}
-	require.Equal(t, 8, c.Volume())
+	require.Equal(t, -27, c.Volume())
+}
+
+func TestRange(t *testing.T) {
+	r1 := Range{0, 1}
+	r2 := Range{1, 2}
+	r3 := Range{2, 3}
+	r4 := Range{0, 3}
+	r5 := Range{1, 4}
+
+	/* end of r1 touch start of r2 */
+	intersect, ok := r1.Intersect(r2)
+	require.True(t, ok)
+	require.Equal(t, Range{1, 1}, *intersect)
+
+	/* ...and the other way around */
+	intersect, ok = r2.Intersect(r1)
+	require.True(t, ok)
+	require.Equal(t, Range{1, 1}, *intersect)
+
+	/* r1 is within r2 touching start */
+	intersect, ok = r1.Intersect(r4)
+	require.True(t, ok)
+	require.Equal(t, r1, *intersect)
+
+	/* ...and the other way around */
+	intersect, ok = r1.Intersect(r4)
+	require.True(t, ok)
+	require.Equal(t, r1, *intersect)
+
+	/* r3 is within r4 touching end */
+	intersect, ok = r3.Intersect(r4)
+	require.True(t, ok)
+	require.Equal(t, r3, *intersect)
+
+	/* ...and the other way around */
+	intersect, ok = r4.Intersect(r3)
+	require.True(t, ok)
+	require.Equal(t, r3, *intersect)
+
+	/* both ranges are the same */
+	intersect, ok = r1.Intersect(r1)
+	require.True(t, ok)
+	require.Equal(t, r1, *intersect)
+
+	/* ...and the other way around */
+	intersect, ok = r1.Intersect(r1)
+	require.True(t, ok)
+	require.Equal(t, r1, *intersect)
+
+	/* both neither intersect nor touch */
+	intersect, ok = r1.Intersect(r3)
+	require.False(t, ok)
+	require.Nil(t, intersect)
+
+	/* ...and the other way around */
+	intersect, ok = r1.Intersect(r3)
+	require.False(t, ok)
+	require.Nil(t, intersect)
+
+	/* r2 is completely within r4 */
+	intersect, ok = r2.Intersect(r4)
+	require.True(t, ok)
+	require.Equal(t, r2, *intersect)
+
+	/* ...and the other way around */
+	intersect, ok = r4.Intersect(r2)
+	require.True(t, ok)
+	require.Equal(t, r2, *intersect)
+
+	/* r4 and r5 overlap */
+	intersect, ok = r4.Intersect(r5)
+	require.True(t, ok)
+	require.Equal(t, Range{1, 3}, *intersect)
+
+	/* ...and the other way around */
+	intersect, ok = r5.Intersect(r4)
+	require.True(t, ok)
+	require.Equal(t, Range{1, 3}, *intersect)
+}
+
+func TestCubeIntersection(t *testing.T) {
+	c1 := Cube{Range{0, 2}, Range{0, 2}, Range{0, 2}, false}
+	c2 := Cube{Range{1, 3}, Range{1, 3}, Range{1, 3}, false}
+	c3 := Cube{Range{1, 2}, Range{1, 2}, Range{1, 2}, false}
+	c4 := Cube{Range{2, 3}, Range{2, 3}, Range{2, 3}, false}
+	c5 := Cube{Range{2, 2}, Range{2, 2}, Range{2, 2}, false}
+
+	/* intersect overlapping cubes */
+	intersect, ok := c1.Intersection(c2)
+	require.True(t, ok)
+	require.Equal(t, c3, *intersect)
+
+	/* ... and the other way around */
+	intersect, ok = c2.Intersection(c1)
+	require.True(t, ok)
+	require.Equal(t, c3, *intersect)
+
+	/* intersection with itself is itself */
+	intersect, ok = c1.Intersection(c1)
+	require.True(t, ok)
+	require.Equal(t, c1, *intersect)
+
+	/* touching cubes have no intersection */
+	intersect, ok = c3.Intersection(c4)
+	require.True(t, ok)
+	require.Equal(t, c5, *intersect)
+
+	/* ... and the other way around */
+	intersect, ok = c4.Intersection(c3)
+	require.True(t, ok)
+	require.Equal(t, c5, *intersect)
 }
